9b9a0edb921e80d17e74f113aa0f490a5a81346a
Uzair Wafai <36201809+uzairwafai@users.noreply.github.com>
2023-11-26 07:09:10 -0800

    Update model/user.js
    Co-authored-by: Asif Iqbal <asifnitsxr@gmail.com>

--- model/user.js
+++ model/user.js
@@ -15,7 +15,7 @@ const schema = new mongoose.Schema({
         type: Number,
         required: [true, 'Mobile number is required']
     },
-    roleID: {
+    roleId: {
         type: Number,
         default: 1
     },
