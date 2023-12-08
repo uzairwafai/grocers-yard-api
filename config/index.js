f4f045c3fbbd92b110d9939c585a6652159ca88a
Uzair Wafai <36201809+uzairwafai@users.noreply.github.com>
2023-12-08 01:18:35 -0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
-  conStr:"mongodb+srv://uzairwafai:uaandws1@cluster0.zvn7ya0.mongodb.net/grocers_yard"
-//   conStr: process.env.conStr,
+  // conStr:"mongodb+srv://uzairwafai:uaandws1@cluster0.zvn7ya0.mongodb.net/grocers_yard"
+  conStr: process.env.conStr,
 };
